0c758d08809180eed7015cd6bc73dbe9d6135eed
scutis <ls3612@ic.ac.uk>
2016-05-10 20:02:53 +0100

    Improved search tool

--- public/js/main.js
+++ public/js/main.js
@@ -16,16 +16,23 @@ $(function() {
 
     $('#search-box').keyup(function(){
         var input = $(this).val();
-        $("#search-res").empty();
-        if (input != "") {
+        if (input != "" && input != " " ) {
             $.post('/search', {search: input}, function (res) {
+                $("#search-res").empty();
                 var output = JSON.parse(res);
                 for (var i = 0; i < output.length; i++) {
-                    $("#search-res").append("<a href='#'>"+output[i]+"</a>");
+                    $("#search-res").append("<a '>"+output[i][1]+" "+output[i][2]+"</a>");
                 }
+                $('#search-res a').click(function(){
+                    $('#search-box').val($(this).text());
+                    $("#search-res").empty();
+                });
             });
         }
+        else
+            $("#search-res").empty();
     });
+
 
     $('#insert').click(function(){
         var input = $('#entry').val();
@@ -37,7 +44,14 @@ $(function() {
     //Login by pressing enter key in password field
     $('#password').keypress(function (e) {
         if (e.which == 13) {
-            $("#login").click()
+            var username = $('#username').val();
+            var password = $('#password').val();
+            $.post('/login', {username: username, password: password}, function (res) {
+                if (res == "Invalid credentials")
+                    $('#info').html(res);
+                else
+                    window.location.href = '/';
+            });
         }
     });
 
--- routes/search.js
+++ routes/search.js
@@ -4,10 +4,10 @@ var mysql = require('../db');
 
 router.post('/', function(req, res) {
     if (req.session.user) {
-        var pattern = req.body.search + '%';
+        var pattern = '%' + mysql.escape(req.body.search) + '%';
 
         mysql.connect(res, function(connection){
-            connection.query('SELECT username FROM users WHERE username LIKE ?', pattern, function (err, result) {
+            connection.query('SELECT id, firstName, lastName FROM users WHERE CONCAT(firstName, " ", lastName) LIKE ?', pattern, function (err, result) {
                 connection.release();
 
                 if (err){
@@ -15,8 +15,9 @@ router.post('/', function(req, res) {
                     return;
                 }
                 var output = [];
-                for (var i = 0; i < result.length; i++)
-                    output.push(result[i].username);
+
+                for (var i = 0; i < Math.min(result.length, 5); i++)
+                    output.push([result[i].id, result[i].firstName, result[i].lastName]);
 
                 res.send(JSON.stringify(output));
 
